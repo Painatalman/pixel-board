11e5c332e278df6ff014d20e97db58afde5374c2
Miguel Costa <maiquedi@gmail.com>
2021-01-09 19:44:17 +0000

    Updates

---
+++ precache-manifest.d0ca62ada640ac85665afa06d2963344.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c47c8b14289f34cbf4c462e3bd2f63b",
+    "url": "/pixel-board/index.html"
+  },
+  {
+    "revision": "2ddc76677a22976ca50d",
+    "url": "/pixel-board/static/css/main.22fe41fd.chunk.css"
+  },
+  {
+    "revision": "3fadc9133d9d3438b422",
+    "url": "/pixel-board/static/js/2.9d3b1846.chunk.js"
+  },
+  {
+    "revision": "2ddc76677a22976ca50d",
+    "url": "/pixel-board/static/js/main.4d15942d.chunk.js"
+  },
+  {
+    "revision": "ae64970e79af979a7ca1",
+    "url": "/pixel-board/static/js/runtime-main.d0c95747.js"
+  }
+]);
